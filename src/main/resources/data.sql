d9dd9b02ea79b37e16a31ea53dfe5051713bfacd
ccastillo32 <cristiancastillo32@gmail.com>
2019-05-15 22:43:15 -0500

    Refactor SQL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,15 +5,15 @@ INSERT INTO tipo_titular (id, nombre, habilitado) VALUES (2, 'JURIDICO', TRUE);
 
 -- Titular (Tabla común)
 
---INSERT INTO titular (cuit, tipo) VALUES ('20-21321345-9', 1);
---INSERT INTO titular (cuit, tipo) VALUES ('20-21345645-8', 1);
---INSERT INTO titular (cuit, tipo) VALUES ('30-70860396-8', 2);
+INSERT INTO titular (cuit, tipo, habilitado) VALUES ('20-21321345-9', 1, TRUE);
+INSERT INTO titular (cuit, tipo, habilitado) VALUES ('20-21345645-8', 1, TRUE);
+INSERT INTO titular (cuit, tipo, habilitado) VALUES ('30-70860396-8', 2, TRUE);
 
 -- Titular físico
 
---INSERT INTO titular_fisico (id, cuit, dni, nombre, apellido ) VALUES (1, '20-21321345-9', '21321345', 'MARIA ANGELA', 'PEREZ POLO' );
---INSERT INTO titular_fisico (id, cuit, dni, nombre, apellido ) VALUES (2, '20-21345645-8', '21345645', 'RAUL ANTONIO', 'VELEZ VELEZ' );
+INSERT INTO titular_fisico (dni, nombre, apellido, titular_id ) VALUES ('21321345', 'MARIA ANGELA', 'PEREZ POLO', 1);
+INSERT INTO titular_fisico (dni, nombre, apellido, titular_id ) VALUES ('21345645', 'RAUL ANTONIO', 'VELEZ VELEZ', 2 );
 
 -- Titular jurídico
 
---INSERT INTO titular_juridico (id, cuit, razon_social, anio_fundacion ) VALUES (3, '30-70860396-8', 'TECSO', 2002 );
+INSERT INTO titular_juridico (razon_social, anio_fundacion, titular_id ) VALUES ('TECSO', 2002, 3 );
